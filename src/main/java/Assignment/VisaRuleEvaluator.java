f87a42d6de8fe92ecb0a07b506d4f39d20c6f7c6
Ved-Kulkarni <136430347+Ved-Kulkarni@users.noreply.github.com>
2026-01-09 17:27:43 +0530

    Fixed matching rules bug

--- src/main/java/Assignment/VisaRuleEvaluator.java
+++ src/main/java/Assignment/VisaRuleEvaluator.java
@@ -53,7 +53,7 @@ public class VisaRuleEvaluator {
         }
 
         if (matches.isEmpty() || visitingCountry == null || passportCountry == null || travelPurpose == null) {
-            return new VisaDecision(true, VisaType.NONE, new ArrayList<>(), 0, List.of("No matching visa rule found")
+            return new VisaDecision(true, VisaType.UNKNOWN, new ArrayList<>(), 0, List.of("No matching visa rule found")
             );
         }
 
